e2ca72ef0016183720e23acf988bcd78d7e5df0f
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-07-25 12:58:55 +0900

    일대일 식별 관계

---
+++ ch07-jpa-mapping/src/main/java/jpabook/mapping/oneToOne/Board.java
@@ -0,0 +1,19 @@
+package jpabook.mapping.oneToOne;
+
+import javax.persistence.*;
+
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "BOARD_ID")
+    private Long id;
+
+    private String title;
+
+    @OneToOne(mappedBy = "board")
+    private BoardDetail boardDetail;
+
+}
+
